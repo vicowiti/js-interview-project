67b08529a7916e052fdc2b32f44a1e32acc32da4
Victor Owiti <viowiti12@gmail.com>
2023-01-12 19:30:51 +0300

    implemented the loading spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ImSpinner9 } from "react-icons/im";
-
+import "./Loader.css";
 const Loader = () => {
   return (
     <div className="loader">
       <div className="spinner">
-        <ImSpinner9 size={35} color="turquoise" />
+        <ImSpinner9 size={50} color="turquoise" className="spinner-child" />
       </div>
     </div>
   );
